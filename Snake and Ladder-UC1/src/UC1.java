16a34444fdd8df2ef3d202380a9378f08c80aecc
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-12 17:42:06 +0530

    UC1

---
+++ Snake and Ladder-UC1/src/UC1.java
@@ -0,0 +1,11 @@
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int position=0;
+		System.out.println("Welcome To Snake And Ladder Game");
+		System.out.println("Player 1 is at start position "+position);
+	}
+
+}
